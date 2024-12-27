7f8c6c08a92372c8ea92e9e1e3dbb9139fe8ab43
Brayan Ceron <brayan.ceron@edunext.co>
2024-12-27 13:35:45 -0500

    refactor: sort institution select alphabetically

--- src/features/Main/data/thunks.js
+++ src/features/Main/data/thunks.js
@@ -1,5 +1,6 @@
 import { logError } from '@edx/frontend-platform/logging';
 import { camelCaseObject } from '@edx/frontend-platform';
+import { sortAlphabetically } from 'react-paragon-topaz';
 import { getInstitutionName } from 'features/Main/data/api';
 import {
   updateSelectedInstitutions,
@@ -15,7 +16,8 @@ function fetchInstitutionData() {
     dispatch(updateSelectedInstitutions({ status: RequestStatus.LOADING }));
     try {
       const { data } = camelCaseObject(await getInstitutionName());
-      dispatch(updateSelectedInstitutions({ status: RequestStatus.SUCCESS, data }));
+      const sortedData = sortAlphabetically(data, 'name');
+      dispatch(updateSelectedInstitutions({ status: RequestStatus.SUCCESS, data: sortedData }));
     } catch (error) {
       dispatch(updateSelectedInstitutions({ status: RequestStatus.ERROR, error }));
       logError(error);
