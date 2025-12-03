e55f01828f840b4884fc0104af93e014bd05a99d
Aura Alba <aura.alba@edunext.co>
2025-12-02 19:09:12 -0500

    feat: change edx to openedx frontend build

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,20 @@
-const { createConfig } = require('@edx/frontend-build');
+const { createConfig } = require('@openedx/frontend-build');
+
 process.env.TZ = 'UTC';
 
 module.exports = createConfig('jest', {
   moduleDirectories: ['node_modules', 'src'],
-  setupFilesAfterEnv: [
-    '<rootDir>/src/setupTest.js',
-  ],
+
+  setupFilesAfterEnv: ['<rootDir>/src/setupTest.js'],
+
   coveragePathIgnorePatterns: [
-    'src/setupTest.js',
+    'src/setupTests.js',
     'src/i18n',
   ],
+
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(css|scss|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
 });
